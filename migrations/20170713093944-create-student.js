4fad6b099bcdea2edae1e70c95183fa10b6a2847
tamastro <rpratama45@gmail.com>
2017-08-15 16:20:57 +0700

    tambah email

--- migrations/20170713093944-create-student.js
+++ migrations/20170713093944-create-student.js
@@ -14,9 +14,6 @@ module.exports = {
       last_name: {
         type: Sequelize.STRING
       },
-      email: {
-        type: Sequelize.STRING
-      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
